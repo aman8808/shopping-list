c23f63b913a45eb5ff940ac42a8e372d603ba449
aman8808 <karabalaev.aman1988@gmail.com>
2022-04-23 11:40:34 +0400

    My project - shopping_list

---
+++ src/main/java/ru/karabalaev/shoppinglist/persist/ShoppingItemRepository.java
@@ -0,0 +1,8 @@
+package ru.karabalaev.shoppinglist.persist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+}
